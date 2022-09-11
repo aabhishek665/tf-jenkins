85d8a3f5a892c488b75e6bec179f5749df94de47
aabhishek665 <79049501+aabhishek665@users.noreply.github.com>
2022-09-11 15:31:39 +0530

    remove SG

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 #Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
+/*resource "aws_security_group" "my_security_group" {
   name        = var.security_group
   description = "security group for Ec2 instance"
  
@@ -33,14 +33,13 @@ resource "aws_security_group" "my_security_group" {
   tags= {
     Name = var.security_group
   }
-}
+}*/
 
 # Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  security_groups= [var.security_group]
   tags= {
     Name = var.tag_name
   }
